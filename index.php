2b5e94301bfbea135cdecc4ad26224b723ad3bf1
Tanapon0207 <80101335+Tanapon0207@users.noreply.github.com>
2021-12-07 10:50:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 Tanapon Rattanamano
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+</body>
+<button id="btnjson1">json1</button>
+<script>
+    console.log("ok")
+    function loadjson(){
+        var URL="http://cj-android-demon.herokuapp.com"
+    }
+    $.get(url,(data,status)=>{
+        //console.log
+        
+    });
+</script>
+</html>
