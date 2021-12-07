83d588b23a7bd56f16e6819aaa708b03efd8a386
Tanapon0207 <80101335+Tanapon0207@users.noreply.github.com>
2021-12-07 11:43:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,46 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+ 
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th><th>First Name</th><th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody id="tblStudent">
+        </tbody>
+    </table>
 </body>
-<button id="btnjson1">json1</button>
-name<span id="fmane"></span><br/>
-sname<span id="lmane"></span>
 <script>
-    /*console.log("ok")
-    function loadjson(){
-        var URL="http://cj-android-demon.herokuapp.com"
-    }
-    $.get(url,(data,status)=>{
-        //console.log
-        
-    });*/
-
-    console.log("ok")
-    function loadJSONobject(){
-        var URL="http://cj-android-demon.herokuapp.com/jacon1.php";
-    
+ 
+function loadJSON(){
+    var url = "https://cj-android-demon.herokuapp.com/json2.php";
     $.getJSON(url)
-    .done((data)=>{
-        console.log(data)
-        console.log(data[2])
-        console.log(data[0].fname)
-        console.log(data[0].lname)
-        
-       /* $("#fname").text(data.fname);
-        $("#lname").text(data.lname);*/
-    })
-    .fail((xhr,status,err)=>{
-        console.log('error')
-    })
-    
-      $(()=>{
-          loadJSONobject();
-      })
-  
+        .done((data)=>{
+            console.log(data);
+            $.each(data,  (k, item)=>{
+                console.log(k);
+                console.log(item);
+                var line = "<tr>";
+                    line += "<td>" + (k+1) + "</td>";
+                    line += "<td>" + item.fname + "</td>";
+                    line += "<td>" + item.lname + "</td>";
+                    line += "</tr>";
+                $("#tblStudent").append(line);
+            });
+        })
+        .fail((xhr, status, err)=>{
+        });
+}
+$(()=>{
+    loadJSON();
+});
 </script>
 </html>
